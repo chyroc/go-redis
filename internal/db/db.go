1a84a3f25df900cf0d5edc98b6081b85c2ff2dcd
chyroc <chyroc@qq.com>
2019-11-20 17:28:41 +0800

    db

---
+++ internal/db/db.go
@@ -0,0 +1,30 @@
+package db
+
+import "github.com/chyroc/go-redis/internal/basetype"
+
+type Database struct {
+	dbnum int // 1 - 16
+	db    []*redisDB
+}
+
+func New() *Database {
+	d := new(Database)
+	d.dbnum = 16
+	d.db = make([]*redisDB, d.dbnum)
+	for i := 0; i < d.dbnum; i++ {
+		d.db[i] = newRedisSB()
+	}
+
+	return d
+}
+
+type redisDB struct {
+	dict    *basetype.Dict // 键值对
+	expires *basetype.Dict // 过期时间 uint64，毫秒时间戳
+}
+
+func newRedisSB() *redisDB {
+	return &redisDB{
+		dict: basetype.NewDict(),
+	}
+}
