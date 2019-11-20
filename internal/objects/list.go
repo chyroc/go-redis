7cc8e37d97c973b35ec369d5418d7f320b686b97
chyroc <chyroc@qq.com>
2019-11-20 15:42:29 +0800

    impl list object

--- internal/objects/list.go
+++ internal/objects/list.go
@@ -9,28 +9,18 @@ type RedisListObject struct {
 }
 
 func NewRedisListObject(l []string) *RedisListObject {
-
-	// 只有满足下面两个条件的时候，才能用 ziplist
-	// 所有字符串长度都小于 64 字节
-	// 元素个数小于 512
-
-	var encoding = RedisObjectEncodingZipList
-	var ll = basetype.NewLinkedList()
-	for _, v := range l {
-		s := basetype.NewSDSWithString(v)
-		ll.AddTail()
-		if encoding == RedisObjectEncodingZipList && s.Len() >= 64 {
-			encoding = RedisObjectEncodingLinkedList
-		}
-	}
-
-	return &RedisListObject{
+	list := &RedisListObject{
 		redisObjectImpl: &redisObjectImpl{
 			type_:    RedisObjectTypeList,
-			encoding: encoding,
-			ptr:      ll,
+			encoding: RedisObjectEncodingZipList,
+			ptr:      basetype.NewLinkedList(),
 		},
 	}
+	for _, v := range l {
+		list.RPush(v)
+	}
+
+	return list
 }
 
 func (r *RedisListObject) Len() uint32 {
@@ -75,10 +65,18 @@ func (r *RedisListObject) Insert(idx uint32, value interface{}) {
 	r.linkedList().Insert(idx, value)
 }
 
-//func (r *RedisListObject) Rem(value interface{}) {
-//	r.linkedList().Insert(idx, value)
-//}
-
 func (r *RedisListObject) linkedList() *basetype.LinkedList {
 	return r.ptr.(*basetype.LinkedList)
+}
+
+// TODO
+func (r *RedisListObject) expencoding(v interface{}) {
+	if r.encoding == RedisObjectEncodingLinkedList {
+		return
+	}
+
+	// 只有满足下面两个条件的时候，才能用 ziplist
+	// 所有字符串长度都小于 64 字节
+	// 元素个数小于 512
+
 }
